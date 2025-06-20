13ffbf72e36fe8e0c730a05c65bbb8ed9935a806
zaron4k <76218026+zaron4k@users.noreply.github.com>
2025-06-20 22:38:09 +0300

    Add files via upload

---
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Numbers from 200 to 500 with exactly 6 divisors:\n";
+
+    for (int num = 200; num <= 500; ++num) {
+        int count = 0;
+
+        for (int i = 1; i <= num; ++i) {
+            int result;
+            __asm {
+                mov eax, num; dividend
+                cdq; sign extend EAX into EDX : EAX
+                idiv i; divide by i
+                mov result, edx; remainder in EDX
+            }
+
+            if (result == 0) {
+                ++count;
+            }
+        }
+
+        if (count == 6) {
+            cout << num << " ";
+        }
+    }
+
+    cout << endl;
+    return 0;
+}
